0a55b679eaa9f559d04fe6c13961e665670b84f0
PavanKumarKillana <pa1officialqueries@gmail.com>
2026-02-26 21:18:54 +0530

    Configured for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<REPO_NAME>/',
+  base: '/portfolio/',
   plugins: [react(), tailwindcss()],
 })
